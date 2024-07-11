e3a8de2b0547d6b7d378c30d9144d46116df65b8
TheCong234 <127668626+TheCong234@users.noreply.github.com>
2024-07-11 11:51:28 +0700

    init entry server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-cong
+import express from "express";
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/home', (req, res)=>{
+    res.send('HOME');
+})
+
+app.listen(3000, ()=>{
+    const PORT = process.env.PORT || 8888;
+    console.log(`SERVER ON PORT: ${PORT}`);
+})
