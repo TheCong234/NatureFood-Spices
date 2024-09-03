98e60efe5101b4bd7f57d17ff5ebddf79f16b285
TheCong234 <127668626+TheCong234@users.noreply.github.com>
2024-09-03 20:42:40 +0700

    checkout new branch

--- client/src/components/pages/HomePage/index.jsx
+++ client/src/components/pages/HomePage/index.jsx
@@ -1,15 +1,9 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 import "../../../assets/styles/home.css";
-
-// import required modules
 import { Pagination, Navigation } from "swiper/modules";
 import { Box } from "@mui/material";
 
