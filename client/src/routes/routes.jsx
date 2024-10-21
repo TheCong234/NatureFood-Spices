b636f3d6058a62862d583b05b02120f8b59736cc
TheCong234 <127668626+TheCong234@users.noreply.github.com>
2024-10-22 00:47:15 +0700

    fix conflict

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -47,12 +47,9 @@ import {
     TrackOrder,
     Checkout,
     Blog,
-<<<<<<< HEAD
     Products,
     Cart,
-=======
     Notification,
->>>>>>> a6686bd7b2123e221353079b8093a7f3532c8a86
 } from "../pages/Customer";
 
 export const router = createBrowserRouter([
@@ -123,7 +120,7 @@ export const router = createBrowserRouter([
                     },
                     {
                         path: "/cart",
-                        element: <Cart />, 
+                        element: <Cart />,
                     },
                     {
                         path: "/notification",
